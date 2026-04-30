7693cd1d19b0844a6e59158a1bf6edf8e1e94633
tamiresteles <satlestamires42@gmail.com>
2026-04-30 18:45:52 -0300

    adiciona testes para nome vazio e preço negativo

--- src/main/java/com/example/Produto.java
+++ src/main/java/com/example/Produto.java
@@ -14,6 +14,17 @@ public class Produto {
         if (preco < 0) {
             throw new IllegalArgumentException("Preço não pode ser negativo!");
         }
-        
+          this.nome = nome;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+}
 
         
--- src/test/java/com/example/ProdutoTest.java
+++ src/test/java/com/example/ProdutoTest.java
@@ -8,6 +8,21 @@ public class ProdutoTest {
     @Test
     void deveCriarProdutoValido() {
         Produto p = new Produto("Café", 10.0);
+
         assertEquals("Café", p.getNome());
         assertEquals(10.0, p.getPreco());
     }
+@Test
+    void naoDevePermitirNomeVazio() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Produto("", 10.0);
+        });
+    }
+
+    @Test
+    void naoDevePermitirPrecoNegativo() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Produto("Café", -5.0);
+        });
+    }
+}
