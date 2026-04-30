8abcea0cf6a665a45bb2c9f827e075f5bfdf329c
tamiresteles <satlestamires42@gmail.com>
2026-04-30 17:21:32 -0300

    cria a clsse Produto

---
+++ src/main/java/com/example/Produto.java
@@ -0,0 +1,29 @@
+package com.example;
+
+public class Produto {
+
+    private String nome;
+    private double preco;
+
+    public Produto(String nome, double preco) {
+
+        if (nome == null || nome.isEmpty()) {
+            throw new IllegalArgumentException("Nome do produto não pode ser vazio!");
+        }
+
+        if (preco < 0) {
+            throw new IllegalArgumentException("Preço não pode ser negativo!");
+        }
+
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+}
