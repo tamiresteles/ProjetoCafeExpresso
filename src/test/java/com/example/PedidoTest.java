80cae0e453dcfacd56a9448e266d5b441359e9f7
tamiresteles <satlestamires42@gmail.com>
2026-04-30 22:03:07 -0300

    soma total com vários produtos

--- src/test/java/com/example/PedidoTest.java
+++ src/test/java/com/example/PedidoTest.java
@@ -29,4 +29,14 @@ void deveSomarDoisProdutos() {
 
     assertEquals(15.0, pedido.calcularTotal());
 }
+@Test
+void deveSomarVariosProdutos() {
+    Pedido pedido = new Pedido();
+
+    pedido.adicionarProduto(new Produto("Café", 10.0));
+    pedido.adicionarProduto(new Produto("Pão", 5.0));
+    pedido.adicionarProduto(new Produto("Leite", 7.0));
+
+    assertEquals(22.0, pedido.calcularTotal());
+}
 }
