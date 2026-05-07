1d3ef57824c31d6e027899e5115e3626e5eb76aa
Gisele <satelesg75@gmail.com>
2026-05-06 22:08:55 -0300

    Projeto limpo sem JDK

---
+++ src/main/java/com/example/ItemPedido.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class ItemPedido {
+
+    private Produto produto;
+    private int quantidade;
+}
+public ItemPedido(Produto produto, int quantidade) {
+    this.produto = produto;
+    this.quantidade = quantidade;
+}
---
+++ src/test/java/com/example/ProdutoTest.java
@@ -0,0 +1,27 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ProdutoTest {
+
+    @Test
+    void deveCriarProdutoValido() {
+       Produto p = new Produto ("café", 10.0);
+        assertEquals("Café", p.getNome());
+        assertEquals(10.0, p.getPreco());
+    }
+@Test
+    void naoDevePermitirNomeVazio() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Produto("", 10.0);
+        });
+    }
+
+    @Test
+    void naoDevePermitirPrecoNegativo() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Produto("Café", -5.0);
+        });
+    }
+}
