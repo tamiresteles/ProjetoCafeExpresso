0964264b8469048a827485005b5675c574d7a373
tamiresteles <satlestamires42@gmail.com>
2026-05-07 15:52:21 -0300

    Implementa cálculo de subtotal em ItemPedido

--- src/main/java/com/example/ItemPedido.java
+++ src/main/java/com/example/ItemPedido.java
@@ -15,4 +15,15 @@ public ItemPedido(Produto produto, int quantidade) {
     this.produto = produto;
     this.quantidade = quantidade;
 }
+   public double calcularSubtotal() {
+        return produto.getPreco() * quantidade;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
 }
