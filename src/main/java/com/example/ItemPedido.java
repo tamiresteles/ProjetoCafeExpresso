82dd1368eddbf795bdcb8c53a3d851ea796c55a8
tamiresteles <satlestamires42@gmail.com>
2026-05-07 15:04:40 -0300

    Resolve merge

--- src/main/java/com/example/ItemPedido.java
+++ src/main/java/com/example/ItemPedido.java
@@ -1,24 +1,11 @@
 package com.example;
-<<<<<<< HEAD
-// cria a classe produto
-=======
-
->>>>>>> 17418ee90c9c60e848eed2269c0b9e2c125f84ff
 public class ItemPedido {
 
     private Produto produto;
     private int quantidade;
-<<<<<<< HEAD
-}
-public ItemPedido(Produto produto, int quantidade) {
-this.produto = produto;
-this.quantidade = quantidade;
-
-=======
 
 public ItemPedido(Produto produto, int quantidade) {
     this.produto = produto;
     this.quantidade = quantidade;
 }
->>>>>>> 17418ee90c9c60e848eed2269c0b9e2c125f84ff
 }
