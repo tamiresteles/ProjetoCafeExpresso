329e0ef1399afbad00d81e96db7f4732ec7825da
tamiresteles <satlestamires42@gmail.com>
2026-05-07 14:37:15 -0300

    Adiciona construtor na classe Produto

--- src/main/java/com/example/ItemPedido.java
+++ src/main/java/com/example/ItemPedido.java
@@ -1,7 +1,12 @@
 package com.example;
-
+// cria a classe produto
 public class ItemPedido {
 
     private Produto produto;
     private int quantidade;
+}
+public ItemPedido(Produto produto, int quantidade) {
+this.produto = produto;
+this.quantidade = quantidade;
+
 }
