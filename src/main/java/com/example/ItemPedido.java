be849ed91bf76241310b4c62e7a18b260c9f4e4c
tamiresteles <satlestamires42@gmail.com>
2026-05-07 15:49:15 -0300

    Adiciona validação de produto e quantidade

--- src/main/java/com/example/ItemPedido.java
+++ src/main/java/com/example/ItemPedido.java
@@ -5,6 +5,13 @@ public class ItemPedido {
     private int quantidade;
 
 public ItemPedido(Produto produto, int quantidade) {
+     if (produto == null) {
+            throw new IllegalArgumentException("Produto não pode ser nulo!");
+        }
+
+        if (quantidade <= 0) {
+            throw new IllegalArgumentException("Quantidade deve ser maior que zero!");
+        }
     this.produto = produto;
     this.quantidade = quantidade;
 }
