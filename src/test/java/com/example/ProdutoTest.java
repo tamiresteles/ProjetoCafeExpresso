6b8e41f29cfe3bd8f0c792ea42bd674dc844d431
tamiresteles <satlestamires42@gmail.com>
2026-05-07 16:47:29 -0300

    remove chave sobrando

--- src/test/java/com/example/ProdutoTest.java
+++ src/test/java/com/example/ProdutoTest.java
@@ -8,7 +8,7 @@ public class ProdutoTest {
     @Test
     void deveCriarProdutoValido() {
        Produto p = new Produto ("café", 10.0);
-        assertEquals("café", p.getNome());
+        assertEquals("afé", p.getNome());
         assertEquals(10.0, p.getPreco());
     }
 @Test
