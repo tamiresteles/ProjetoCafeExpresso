350887a217f8a4dad703a432022c901f13c8e276
Gisele <satelesg75@gmail.com>
2026-05-21 20:52:28 -0300

    Corrige validações e atualiza classe ItemPedido

--- src/main/java/com/example/ItemPedido.java
+++ src/main/java/com/example/ItemPedido.java
@@ -1,10 +1,12 @@
 package com.example;
+
 public class ItemPedido {
 
     private Produto produto;
     private int quantidade;
 
     public ItemPedido(Produto produto, int quantidade) {
+
         if (produto == null) {
             throw new IllegalArgumentException("Produto não pode ser nulo!");
         }
@@ -12,10 +14,12 @@ public class ItemPedido {
         if (quantidade <= 0) {
             throw new IllegalArgumentException("Quantidade deve ser maior que zero!");
         }
+
         this.produto = produto;
         this.quantidade = quantidade;
-}
-   public double calcularSubtotal() {
+    }
+
+    public double calcularSubtotal() {
         return produto.getPreco() * quantidade;
     }
 
